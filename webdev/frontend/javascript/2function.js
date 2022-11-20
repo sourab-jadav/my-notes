f079ecd2eac6e60152504ceceb2ac44d2cd2e83e
sourab-jadav <sourabhjadav7@gmail.com>
2022-11-20 12:56:31 +0530

    blah

---
+++ webdev/frontend/javascript/2function.js
@@ -0,0 +1,26 @@
+/ Default Parameters
+const bookings = [];
+
+const createBooking = function ( flightNum, numPassengers = 1, price = 199 * numPassengers) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);    --making undefined will simply take the default vlue of the parameter
+
+
+**we pass a reference to a function not by reference
